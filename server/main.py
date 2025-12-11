bd2631c67efc2560ec17fa9128e19714831014d0
daibanezb-cpu <daibanezb@unal.edu.co>
2025-12-11 13:10:36 -0500

    Update main.py

--- server/main.py
+++ server/main.py
@@ -7,6 +7,7 @@ from typing import List, Optional
 from sqlalchemy import func
 from typing import List, Optional, Any
 import asyncio
+import pytz 
 
 
 from . import database, models, schemas, crud, auth, websocket_manager, config
@@ -57,7 +58,8 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
 async def receive_measurement(payload: schemas.MeasurementPayload, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     # Validate timestamp
     if payload.timestamp is None:
-        timestamp = datetime.utcnow()
+        bogota = pytz.timezone("America/Bogota")
+        timestamp = datetime.now(bogota)
     else:
         timestamp = payload.timestamp
 
@@ -154,6 +156,7 @@ async def ws_realtime(ws: WebSocket):
 @app.get("/api/profile", response_model=schemas.UserOut)
 def profile(current_user = Depends(auth.get_current_user)):
     return current_user
+
 
 
 
